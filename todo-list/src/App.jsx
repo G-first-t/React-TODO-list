fdbd7e9b741ea49535a968091f01ab9d0af15947
Gregory kimbira <gregorykimbira7@gmail.com>
2024-05-04 18:44:02 +0300

    creating a form

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -3,6 +3,7 @@ import TodoApp from "./components/Todo";
 const App=()=>{
   return(
     <div>
+      <h1>ToDo List</h1>
      <TodoApp/>
     </div>
   );
--- todo-list/src/components/Todo.jsx
+++ todo-list/src/components/Todo.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 const TodoApp=()=>{
+    const [items,setItems]=useState("");
+    const handler=(e)=>{
+         setItems([
+            ...items,
+         ])
+    }
  return(
     <div>
-        
+        <div>
+            <form action="">
+                <input type="text" placeholder="add items" value={items}/>
+                <button onClick={handler}>Add</button>
+            </form>
+            <div>
+          
+            </div>
+        </div>
     </div>
  );
 }
