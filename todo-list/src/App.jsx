2c073f77d707ef30b8cafcb2009bf16f98b77e02
Gregory kimbira <gregorykimbira7@gmail.com>
2024-05-17 11:59:09 +0300

    stylin buttons

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -19,13 +19,13 @@ return(
 
              </div>
              <div class="btn-area">
-                    <button>Todo</button>
-                    <button>Completed</button>
+                    <button className="secondaryBtn">Todo</button>
+                    <button className="secondaryBtn">Completed</button>
              </div>
                <div className="todo-list">
                   <div className="todo-list-item">
                     <h3>Task1</h3>
-                    <p>Description</p>
+                    <p>Description </p>
                   </div>
               </div>
            </div>
